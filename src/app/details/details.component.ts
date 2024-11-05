b0b59826e1bc2cc1ef449e550f23b9a8b05e3be4
martinezgomez34 <martinezgomez.edu34@gmail.com>
2024-11-04 20:57:03 -0600

    newbranch

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,28 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FakestoreService } from '../service/fakestore.service';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [
-
+    CommonModule,
   ],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
 })
 export class DetailsComponent implements OnInit{
-  ability: any[] = [];
-  
-  constructor(private pokemonService: FakestoreService) { }
+  pokemon: any;
+
+  constructor(private route: ActivatedRoute, private pokemonService: FakestoreService) { }
 
   ngOnInit(): void {
-    this.Ability();
-  }
-
-  Ability(): void {
-    this.pokemonService.getAbility().subscribe(response => {
-      this.ability = response.results;
+    const name = this.route.snapshot.paramMap.get('name');
+    
+    this.pokemonService.getPokemonDetails(name!).subscribe(data => {
+      this.pokemon = data;
     });
   }
 
--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,44 +1,22 @@
 import { Component , OnInit} from '@angular/core';
 import { FakestoreService } from '../service/fakestore.service';
-import { CommonEngine } from '@angular/ssr';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   templateUrl: './pokemon.component.html',
   styleUrl: './pokemon.component.scss'
 })
 export class PokemonComponent implements OnInit{
-  pokemons: any[] = [];
-  games: any[] = [];
-  ability: any[] = [];
-
-  constructor(private pokemonService: FakestoreService) { }
-
-  ngOnInit(): void {
-    this.Pokemons();
-    this.Games();
-    this.Ability();
-  }
-
-  Pokemons(): void {
-    this.pokemonService.getPokemons().subscribe(response => {
-      this.pokemons = response.results;
-    });
-  }
-  Games(): void {
-    this.pokemonService.getGames().subscribe(response => {
-      this.games = response.results;
-    })
-  }
-  Ability(): void {
-    this.pokemonService.getAbility().subscribe(response => {
-      this.ability = response.results;
-    });
-  }
-
+  pokemons: any[] = []; 
+  favorites: Set<string> = new Set(); 
+  constructor(private pokemonService: FakestoreService, private router: Router) { } 
+  
+    ngOnInit(): void { this.pokemonService.getPokemons().subscribe(data => { this.pokemons = data.results; }); } toggleFavorite(pokemon: any): void { if (this.favorites.has(pokemon.name)) { this.favorites.delete(pokemon.name); } else { this.favorites.add(pokemon.name); } } navigateToDetail(name: string): void { this.router.navigate(['/pokemon', name]); }
 }
